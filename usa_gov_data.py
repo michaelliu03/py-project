dcf11d1eda6eda867979c708050f11eabd9d95a1
michaelliu03 <841412988@qq.com>
2018-06-21 00:52:23 +0800

    some python practice and learn project

--- usa_gov_data.py
+++ usa_gov_data.py
@@ -2,6 +2,11 @@ import codecs
 import json
 from collections import defaultdict
 from collections import Counter
+from pandas import  DataFrame,Series
+import pandas as pd
+import  numpy as np
+import matplotlib.pyplot as plt
+
 
 
 path = './data/usagov_bitly_data2012-03-16-1331923249.txt'
@@ -31,17 +36,53 @@ def top_counts(count_dict ,n=10):
 
 def process():
     records,time_zones = read_usa_recods()
-    print(time_zones[:10])
+    #print(time_zones[:10])
     count = get_counts(time_zones)
-    print("时区的数量", count['America/New_York'])
-    print("the total num of time_zones", len(time_zones))
-    print(top_counts(count,5))
+    #print("时区的数量", count['America/New_York'])
+    #print("the total num of time_zones", len(time_zones))
+    #print(top_counts(count,5))
     topcounts = top_counts(count, n=10)
-    for item in topcounts:
-        print(item)
+    #for item in topcounts:
+    #    print(item)
 
     topcounts_way = Counter(time_zones)
-    print(topcounts_way.most_common(10))
+    #print(topcounts_way.most_common(10))
+
+    frame = DataFrame(records)
+    #print(frame.head(5))
+    tz_counts = frame['tz'].value_counts()
+    #print(tz_counts[:10])
+    clean_tz = frame['tz'].fillna('Missing')
+    clean_tz[clean_tz == ''] = 'UnKnown' ## 数据补齐
+    tz_counts =clean_tz.value_counts()
+    #print(tz_counts[:10])
+    #draw_bar_analysis('barh',tz_counts,10)
+
+    sresult= Series([x.split()[0] for x in frame.a.dropna()])
+    #print(sresult.head(5))
+    sresult_count_only8 = sresult.value_counts()[:8]
+    #print(sresult_count_only8)
+    cframe = frame[frame.a.notnull()]
+    #print(type(cframe))
+    operating_system = np.where(cframe['a'].str.contains('Windows'),'Windows','Not Windows')
+    #print(operating_system[:5])
+    by_tz_os = cframe.groupby(['tz', operating_system])
+    agg_counts = by_tz_os.size().unstack().fillna(0)
+    #print(agg_counts[:10])
+    indexer = agg_counts.sum(1).argsort()
+    #print(indexer[:10])
+    count_subset = agg_counts.take(indexer)[-10:]
+    #print(count_subset)
+    count_subset.plot(kind='barh', stacked=True)
+    plt.show()
+    normed_subset = count_subset.div(count_subset.sum(1), axis=0)
+    normed_subset.plot(kind='barh', stacked=True)
+    plt.show()
+
+def draw_bar_analysis(type,tz_counts,n):
+    plt.figure(figsize=(10, 6))
+    tz_counts[:n].plot(kind=type, rot=0)
+    plt.show()
 
 
 if __name__ == "__main__":
