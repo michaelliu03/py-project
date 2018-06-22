17d1f8d7e778dda7929994127334bff9b5e70865
michaeliu03 <841412988@qq.com>
2018-06-22 09:00:49 +0800

    添加了normalization 处理

--- classify/classifer.py
+++ classify/classifer.py
@@ -24,8 +24,13 @@ def process():
     corpus, labels = get_data()  # 获取数据集
     print("总的数据量:", len(labels))
     corpus, labels = remove_empty_docs(corpus, labels)
-    print('样本之一:', corpus[10])
-    print('样本的label:', labels[10])
+    label_name_map = ["垃圾邮件", "正常邮件"]
+
+    # 对数据进行划分
+    train_corpus, test_corpus, train_labels, test_labels = prepare_datasets(corpus,
+                                                                            labels,
+                                                                            test_data_proportion=0.3)
+
 
 if  __name__ == "__main__":
     process()
